c1b4a836fdd7b5bbbec3113378bc5ee95ee723e1
zainsubhani <96117969+zainsubhani@users.noreply.github.com>
2021-12-15 10:20:55 -0800

    commit

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,8 @@
+import './search-box.styles.css';
+ export const SearchBox = ({placeholder, handlechange}) => (
+    <input className="search"
+     type='search' 
+     placeholder={placeholder}
+     onChange={handlechange}
+      /> 
+ )
